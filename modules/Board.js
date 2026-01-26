4633799a3e28c786120c3abe2a9ae4c08e3f8ebf
bastienbo <109655148+bastienboLAG@users.noreply.github.com>
2026-01-26 20:09:16 +0100

    Update Board.js

--- modules/Board.js
+++ modules/Board.js
@@ -1,23 +1,14 @@
-// modules/Board.js
 export class Board {
     constructor() {
-        this.placedTiles = {}; // Format : { "0,0": tuileObjet, "1,0": tuileObjet }
+        this.placedTiles = {}; 
     }
 
-    // Ajouter une tuile
     addTile(x, y, tile) {
         this.placedTiles[`${x},${y}`] = tile;
     }
 
-    // Vérifier les voisins pour savoir si on peut poser la tuile
-    getNeighbor(x, y, direction) {
-        // direction: 0=nord, 1=est, 2=sud, 3=ouest
-        const neighbors = {
-            0: `${x},${y-1}`, // Nord
-            1: `${x+1},${y}`, // Est
-            2: `${x},${y+1}`, // Sud
-            3: `${x-1},${y}`  // Ouest
-        };
-        return this.placedTiles[neighbors[direction]];
+    isFree(x, y) {
+        // Renvoie true si AUCUNE tuile n'existe à ces coordonnées
+        return this.placedTiles[`${x},${y}`] === undefined;
     }
 }
